4762eeb4a13713ce8df9a33fb1d41bf75da5a5cc
jerico-esguerra <jericoesguerra212@outlook>
2023-01-14 13:56:26 -0500

    SearchExercises minor style changes

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -9,20 +9,20 @@ const SearchExercises = () => {
   }
 
   return (
-    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
-      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
+    <Stack alignItems="center" mt="2.313rem" justifyContent="center" p="1.25rem">
+      <Typography fontWeight={700} sx={{ fontSize: { lg: '2.75rem', xs: '1.875rem' } }} mb="3.063rem" textAlign="center">
         Awesome Exercises You <br /> Should Know
       </Typography>
-      <Box position="relative" mb="72px">
+      <Box position="relative" mb="4.5rem">
         <TextField
-          height="76px"
-          sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '1170px', xs: '350px' }, backgroundColor: '#fff', borderRadius: '40px' }}
+          height="4.75rem"
+          sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '0.25rem' }, width: { lg: '73.125rem', xs: '21.875rem' }, backgroundColor: '#fff', borderRadius: '2.5rem' }}
           value={search}
           onChange={(e) => setSearch(e.target.value.toLowerCase())}
           placeholder="Search Exercises"
           type="text"
         />
-        <Button className="search-btn" sx={{ bgcolor: '#FF2625', color: '#fff', textTransform: 'none', width: { lg: '173px', xs: '80px' }, height: '56px', position: 'absolute', right: '0px', fontSize: { lg: '20px', xs: '14px' } }} onClick={handleSearch}>
+        <Button className="search-btn" sx={{ bgcolor: '#FF2625', color: '#fff', textTransform: 'none', width: { lg: '10.813rem', xs: '5rem' }, height: '3.5rem', position: 'absolute', right: '0px', fontSize: { lg: '1.25rem', xs: '0.875rem' } }} onClick={handleSearch}>
           Search
         </Button>
       </Box>
